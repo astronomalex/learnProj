8d4e3f276389b49939bbe25d9894a428ae04051d
astronomalex <52239193+astronomalex@users.noreply.github.com>
2019-11-28 18:02:23 +0300

    12 глава середина

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Ricipe',
+      'This is a simply a test',
+      'https://www.gotemplates.store/wp-content/uploads/Rn1Ke6/recipe-cards.jpg'),
+    new Recipe('Test Ricipe',
+      'This is a simply a test',
+      'https://www.gotemplates.store/wp-content/uploads/Rn1Ke6/recipe-cards.jpg')
+  ];
 
   constructor() { }
 
