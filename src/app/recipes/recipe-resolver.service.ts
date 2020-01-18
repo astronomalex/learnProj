cbc08aa6f301f4c6eadcc557755adde05acf4499
astronomalex <52239193+astronomalex@users.noreply.github.com>
2020-01-18 03:22:27 +0300

    20.9 end

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -2,15 +2,24 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {Recipe} from './recipe.model';
 import {DataStorageSevice} from '../shared/data-storage.sevice';
+import {RecipeService} from './recipe.service';
 
 
 @Injectable({
   providedIn : 'root'
 })
-export class RecipeResolverServise implements Resolve<Recipe[]> {
-  constructor(private dataStorageSevice: DataStorageSevice) {}
+export class RecipeResolverService implements Resolve<Recipe[]> {
+  constructor(private dataStorageSevice: DataStorageSevice,
+              private recipeService: RecipeService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageSevice.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorageSevice.fetchRecipes();
+    } else {
+      return recipes;
+    }
+
   }
 }
