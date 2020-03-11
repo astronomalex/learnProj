81f39dce6398c3538aec8ffe2908741052420b77
astronomalex <52239193+astronomalex@users.noreply.github.com>
2020-03-11 23:18:31 +0300

    state management 36

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Recipe} from '../recipe.model';
-import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {map} from 'rxjs/operators';
+
+import {Recipe} from '../recipe.model';
+import * as fromApp from '../../store/app.reducer';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,18 +17,19 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit() {
-    this.subscription = this.recipeService.recipesChanged.subscribe(
-      (recipes: Recipe[]) => {
-        this.recipes = recipes;
-      }
+    this.subscription = this.store.select('recipes')
+      .pipe(map(recipesState => recipesState.recipes))
+        .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipes = recipes;
+        }
     );
-    this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
